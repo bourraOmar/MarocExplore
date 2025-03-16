45a329f33392c9dd1cbe910282acc943cad27dc6
bourraOmar <bwrtmr@gmail.com>
2025-03-16 18:13:50 +0000

    add function stor destination

--- app/Http/Controllers/DestinationController.php
+++ app/Http/Controllers/DestinationController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class DestinationController extends Controller
 {
-    
+    public function store(Request $request, $itineraryId)
+    {
+        $itinerary = itinerary::where('user_id', Auth::id())->find($itineraryId);
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'lodging' => 'nullable|string',
+            'places_to_visit' => 'nullable|array',
+        ]);
+
+        $destination = $itinerary->destinations()->create($validated);
+
+        return response()->json($destination, 201);
+    }
 }
