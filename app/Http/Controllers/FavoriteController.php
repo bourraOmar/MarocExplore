c40be287e1bd4666df7d7b1494c0cf5e3c91e101
bourraOmar <bwrtmr@gmail.com>
2025-03-16 18:27:09 +0000

    add function stor favorite

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class FavoriteController extends Controller
 {
-    //
+    public function store($id){
+
+        $itinerary = DB::table('itineraries')->where('id', $id)->exists();
+
+        if(!$itinerary){
+            return response([
+                'message' => "itinerary doesn't exist!"
+            ]);
+        }
+
+        $is_exists = DB::table('favorites')->where('itineraries_id', $id)->where('user_id', Auth::id())->exists();
+
+        if($is_exists){
+            return response([
+                'message' => "already added!"
+            ]);
+        }
+
+        $favori = Auth::user()->favorites()->create(['itineraries_id' => $id]);
+
+        return response([
+            'message' => 'the itinerary added to you favorite list successfuly'
+        ]);
+    }
 }
