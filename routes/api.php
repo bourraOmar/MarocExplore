81c5f20a82bbba6ea76ca97fcb05491145e38a61
bourraOmar <bwrtmr@gmail.com>
2025-03-19 11:37:12 +0000

    add test Store

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::resource('products', ProductController::class);
 
 // Route::get('/products', [ProductController::class, 'index']); 
 // Route::post('/products', [ProductController::class, 'store']);
